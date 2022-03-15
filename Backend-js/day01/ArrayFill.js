c1856c8f9c47f309524455c996159bebd18718cd
leokim1178 <leo3179@naver.com>
2022-03-15 11:00:00 +0000

    day 1-3

--- Backend-js/day01/ArrayFill.js
+++ Backend-js/day01/ArrayFill.js
@@ -2,7 +2,10 @@
 // 뒤의 4자리를 "*"로 바꿔서 리턴해주세요.
 
 function solution(phone) {
-  let arr = phone.split('');
+  let arr = phone.split(''); //split은 ()안에 들어가는값을 기준으로 배열을 나눈다
+  console.log(arr)
+  const result = arr.fill('*',7).join("") //join은 ()안에 들어갈 문자를 기준으로 합친다
+  return result
   // 여기에서 작성해주세요.
   
 }
--- Backend-js/day01/ArrayIncludes.js
+++ Backend-js/day01/ArrayIncludes.js
@@ -3,11 +3,15 @@
 // 백엔드 직무 지원자 명단이 주어질 때,
 // 중복 지원한 사람을 찾아 배열에 담아 리턴해주세요.
 function solution(front, back) {
-
-  
+    for(i=0;i<front.length;i++){
+    const result1=front.includes(back[i])
+    if(result1===true){
+        return [back[i]];}
+    }
 }
 
 const front = ['아라', '세준', '재환'];
 const back = ['지연', '아라', '재훈'];
 const result = solution(front, back);
+
 console.log("결과", result); // ["아라"]
