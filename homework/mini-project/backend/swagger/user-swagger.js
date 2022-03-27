af9cd34b1ac7f3194afc5a7ac9ba75a88812e600
김태영 <leokim@gimtaeyeong-ui-MacBookAir.local>
2022-03-27 12:46:30 +0900

    day10 with mini project-finish

--- homework/mini-project/backend/swagger/user-swagger.js
+++ homework/mini-project/backend/swagger/user-swagger.js
@@ -1,65 +1,44 @@
 /**
  * @swagger
- * /boards:
- *      get:
- *          summary: 게시글 가져오기
- *          tags: [Board]
- *          parameters:
- *              - in: query
- *                name: number
- *                type: int
+ * /user:
+ *      post:
+ *          summary: 유저 추가하기
+ *          tags: [회원가입]
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              user:          
+ *                                 type: object                                              
+ *                                 properties:                                              
+ *                                  name:
+ *                                      type: string
+ *                                  email:
+ *                                      type: string
+ *                                  phone:
+ *                                      type: string
+ *                                  personal:
+ *                                      type: string
+ *                                  prefer:
+ *                                      type: string
+ *                                  pwd:
+ *                                      type: string
  *          responses:
  *              200:
- *                  description: 성공
+ *                  description: user의 _id 리턴
  *                  content:
  *                      application/json:
  *                          schema:
- *                            type: array
- *                            items:
- *                              type: object
- *                              properties:
- *                                  number:
- *	                                    type: int
- *	                                    example: 1
- *                                  writer:
- *	                                    type: string
- *	                                    example: 철수
- *                                  title:
- *	                                    type: string
- *	                                    example: 제목입니다~~~
- *                                  contents:
- *	                                    type: string
- *	                                    example: 내용이에요!!!
- */
-
-/**
- * @swagger
- * /boards:
- *       post:
- *          summary: 게시글 등록하기
- *          tags: [Board]
- *          parameters:
- *              - in: query
- *                name: writer
- *                type: string
- *              - in: query
- *                name: password
- *                type: string
- *              - in: query
- *                name: title
- *                type: string
- *              - in: query
- *                name: contents
- *                type: string
- *          responses:
- *              200:
- *                  description: 성공
+ *                            type: string
+ *                            example: 623f17e89d3fb761d403fac9
+ *              422:
+ *                  description: 핸드폰 번호 인증 실패
  *                  content:
  *                      application/json:
  *                          schema:
- *                              type: object
- *                              properties:
- *                                  message:
- *	                                    type: string
- *	                                    example: 게시물이 정상적으로 등록되었습니다.
+ *                            type: string
+ *                            example: "에러!!핸드폰번호가 인증되지않았습니다!"
  */
