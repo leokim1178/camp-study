d2809a050a987d60ecc1276ff9654b754ea9c221
leokim1178 <leo3179@naver.com>
2022-04-01 12:53:21 +0900

    day15-2

--- Progammers-Algorithm/day15/JadenCase 문자열 만들기.js
+++ Progammers-Algorithm/day15/JadenCase 문자열 만들기.js
@@ -1,4 +1,4 @@
-function solution(s) {
+function solution1(s) {
   let arr = s.split(" ").filter((x) => {
     return x !== "";
   });
@@ -11,4 +11,35 @@ function solution(s) {
     }
   });
   return answer.join(" ");
+}
+
+function solution2(s) {
+  s = s.toLowerCase();
+  let idx = 0;
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    if (letter === " ") {
+      //공백이라면 idx를 초기화한다
+      idx = 0;
+    } else {
+      if (idx === 0) {
+        //단어의 앞부분을 체크한다
+        letter = s[i].toUpperCase();
+      }
+      idx++;
+    }
+    answer += letter;
+  }
+  return answer;
+}
+
+function solution3(s) {
+  s = s
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word !== "" ? word[0].toUpperCase() + word.substring(1) : word;
+    });
+  return s.join(" ");
 }
--- Progammers-Algorithm/day15/음양 더하기.js
+++ Progammers-Algorithm/day15/음양 더하기.js
@@ -1,4 +1,4 @@
-function solution(absolutes, signs) {
+function solution1(absolutes, signs) {
   var answer = 0;
   let arr = new Array(signs.length);
   for (i = 0; i < signs.length; i++) {
@@ -12,3 +12,35 @@ function solution(absolutes, signs) {
 
   return answer;
 }
+
+function solution2(absolutes, signs) {
+  var answer = 0;
+  let arr = new Array(signs.length);
+  absolutes.forEach((x, i) => {
+    arr[i] = [x, signs[i]];
+    if (arr[i][1]) {
+      answer += x;
+    } else {
+      answer -= x;
+    }
+  });
+  return answer;
+}
+
+function solution3(absolutes, signs) {
+  var answer = 0;
+  let arr = new Array(signs.length);
+  absolutes.forEach((x, i) => {
+    arr[i] = [x, signs[i]];
+    arr[i][1] ? (answer += x) : (answer -= x);
+  });
+  return answer;
+}
+
+function solution4(absolutes, signs) {
+  const answer = absolutes.reduce((acc, cur, i) => {
+    return acc + (signs[i] ? cur : -cur);
+  }, 0);
+  return answer;
+}
+//reduce 사용
