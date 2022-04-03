3798e1e84d67e6726ba7f3fbc024f231eed86105
leokim1178 <leo3179@naver.com>
2022-04-03 22:25:46 +0900

    day15-weekend-4

--- Progammers-Algorithm/day21/시저 암호.js
+++ Progammers-Algorithm/day21/시저 암호.js
@@ -1,48 +1,16 @@
 function solution(s, n) {
-  var answer = "";
   const arr = [...s];
-
-  arr.forEach((x, i) => {
-    if (x === " ") {
-      answer += " ";
-    } else if (s.charCodeAt(i) == 90 || s.charCodeAt(i) == 122) {
-      answer += String.fromCharCode(s.charCodeAt(i) - 26 + (n % 25));
-    } else {
-      answer += String.fromCharCode(s.charCodeAt(i) + (n % 25));
-    }
-  });
-
-  return answer;
-}
-
-function solution(s, n) {
-  const arr = [...s];
-  const answer = arr.reduce((acc, cur, i) => {
-    if (cur === " ") {
-      return acc + cur;
-    } else if (s.charCodeAt(i) == 90 || s.charCodeAt(i) == 122) {
-      return acc + String.fromCharCode(s.charCodeAt(i) - 26 + (n % 25));
-    } else {
-      return acc + String.fromCharCode(s.charCodeAt(i) + (n % 25));
-    }
-  }, "");
-
-  return answer;
-}
-
-function solution(s, n) {
-  const arr = [...s];
-  const answer = arr.reduce((acc, cur, i) => {
-    if (cur == " ") {
-      return acc + cur;
+  let answer = arr.map((alp, i) => {
+    if (alp == " ") {
+      return alp;
     } else if (
-      (s.charCodeAt(i) + n > 90 && s.charCodeAt(i) + n < 97) ||
+      (s.charCodeAt(i) + n > 90 && s.charCodeAt(i) < 91) ||
       s.charCodeAt(i) + n > 122
     ) {
-      return acc + String.fromCharCode(s.charCodeAt(i) - 26 + n);
+      return String.fromCharCode(s.charCodeAt(i) - 26 + n);
     } else {
-      return acc + String.fromCharCode(s.charCodeAt(i) + n);
+      return String.fromCharCode(s.charCodeAt(i) + n);
     }
-  }, "");
-  return answer;
+  });
+  return answer.join("");
 }
--- Progammers-Algorithm/day22/숫자 문자열과 영단어.js
+++ Progammers-Algorithm/day22/숫자 문자열과 영단어.js
@@ -1,4 +1,30 @@
-function solution(nums) {
-  var answer = 0;
-  return answer;
+function solution(s) {
+  let arr = [];
+  let eng = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let str = "";
+
+  for (i = 0; i < s.length; i++) {
+    if (isNaN(s[i])) {
+      str += s[i];
+      if (eng.includes(str)) {
+        arr.push(eng.indexOf(str));
+        str = "";
+      }
+    } else if (!isNaN(s[i])) {
+      arr.push(s[i]);
+    }
+  }
+  return Number(arr.join(""));
 }
