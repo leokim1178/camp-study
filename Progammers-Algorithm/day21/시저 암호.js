85d57e86e729d667021a52c0519c7d13ea6262fc
leokim1178 <leo3179@naver.com>
2022-04-03 17:07:34 +0900

    day15-weekend-3

--- Progammers-Algorithm/day21/시저 암호.js
+++ Progammers-Algorithm/day21/시저 암호.js
@@ -1,4 +1,48 @@
-function solution(nums) {
-  var answer = 0;
+function solution(s, n) {
+  var answer = "";
+  const arr = [...s];
+
+  arr.forEach((x, i) => {
+    if (x === " ") {
+      answer += " ";
+    } else if (s.charCodeAt(i) == 90 || s.charCodeAt(i) == 122) {
+      answer += String.fromCharCode(s.charCodeAt(i) - 26 + (n % 25));
+    } else {
+      answer += String.fromCharCode(s.charCodeAt(i) + (n % 25));
+    }
+  });
+
+  return answer;
+}
+
+function solution(s, n) {
+  const arr = [...s];
+  const answer = arr.reduce((acc, cur, i) => {
+    if (cur === " ") {
+      return acc + cur;
+    } else if (s.charCodeAt(i) == 90 || s.charCodeAt(i) == 122) {
+      return acc + String.fromCharCode(s.charCodeAt(i) - 26 + (n % 25));
+    } else {
+      return acc + String.fromCharCode(s.charCodeAt(i) + (n % 25));
+    }
+  }, "");
+
+  return answer;
+}
+
+function solution(s, n) {
+  const arr = [...s];
+  const answer = arr.reduce((acc, cur, i) => {
+    if (cur == " ") {
+      return acc + cur;
+    } else if (
+      (s.charCodeAt(i) + n > 90 && s.charCodeAt(i) + n < 97) ||
+      s.charCodeAt(i) + n > 122
+    ) {
+      return acc + String.fromCharCode(s.charCodeAt(i) - 26 + n);
+    } else {
+      return acc + String.fromCharCode(s.charCodeAt(i) + n);
+    }
+  }, "");
   return answer;
 }
