4ead4b13ce4689370cd0afd3b634cc48e7bf80c5
leokim1178 <leo3179@naver.com>
2022-04-04 13:01:51 +0900

    day16-2

--- Progammers-Algorithm/day16/2016년.js
+++ Progammers-Algorithm/day16/2016년.js
@@ -1,4 +1,4 @@
-function solution(a, b) {
+function solution1(a, b) {
   const day = new Date(`2016.${a}.${b}`).getDay();
   if (day === 0) {
     return "SUN";
@@ -15,4 +15,58 @@ function solution(a, b) {
   } else if (day === 6) {
     return "SAT";
   }
+}
+
+function solution2(a, b) {
+  const w = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const day = new Date(`2016.${a}.${b}`).getDay();
+  for (i = 0; i < w.length; i++) {
+    if (day === i) {
+      return w[i];
+    }
+  }
+}
+
+//solution3
+const month = {
+  1: 31,
+  2: 29,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
+const week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+
+function solution3(a, b) {
+  //a월 b일까지 해당되는 모든 일수를 저장
+  let answer = 0;
+  for (let i = 1; i < a; i++) {
+    //a월 전까지의 모든 월의 일수를 더한다
+    answer += month[i];
+  }
+  answer += b - 1;
+  answer = week[answer % 7];
+  return answer;
+}
+
+function solution4(a, b) {
+  const answer = new Array(a).fill(1).reduce((acc, cur, i) => {
+    const mn = cur + i;
+    return (
+      acc +
+      (mn !== a
+        ? // 이전 월일 경우
+          month[mn]
+        : //해당 월일 경우
+          b - 1)
+    );
+  }, 0);
+  return week[answer % 7];
 }
