b13c1d8a725a92245335a06276c23148097b2a2d
leokim1178 <leo3179@naver.com>
2022-04-06 14:47:28 +0900

    day18-alg

--- Progammers-Algorithm/day18/완주하지 못한 선수.js
+++ Progammers-Algorithm/day18/완주하지 못한 선수.js
@@ -7,3 +7,20 @@ function solution(a, b) {
     }
   }
 }
+
+function solution2(a, b) {
+  for (let i = 0; i < b.length; i++) {
+    if (a.includes(b[i])) {
+      a.splice(a.indexOf(b[i]), 1);
+    }
+  }
+  return a[0];
+}
+
+function solution(a, b) {
+  a.sort();
+  b.sort();
+  return a.filter((name, i) => {
+    return name !== b[i];
+  })[0];
+}
