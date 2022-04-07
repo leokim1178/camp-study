f29c5a2e4e4421d4372694b28ae0d1b3d1dd3734
leokim1178 <leo3179@naver.com>
2022-04-07 11:22:57 +0900

    day19

--- Progammers-Algorithm/day22/소수 만들기.js
+++ Progammers-Algorithm/day22/소수 만들기.js
@@ -1,4 +1,19 @@
 function solution(nums) {
-  var answer = 0;
-  return answer;
+  let arr = [];
+
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      for (k = j + 1; k < nums.length; k++) {
+        arr.push(nums[i] + nums[j] + nums[k]);
+      }
+    }
+  }
+
+  const result = [...arr].filter((x) => {
+    for (i = 2; i < x; i++) {
+      if (x % i == 0) return false;
+    }
+    return true;
+  });
+  return result.length;
 }
