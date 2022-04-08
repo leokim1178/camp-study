95252495f3f441ed487b8206432aa6a9957dfeeb
leokim1178 <leo3179@naver.com>
2022-04-08 12:44:01 +0900

    day20

--- Progammers-Algorithm/day23/실패율.js
+++ Progammers-Algorithm/day23/실패율.js
@@ -1,4 +1,29 @@
-function solution(nums) {
-  var answer = 0;
-  return answer;
+function solution(N, stages) {
+  let arr = new Array(N).fill(0);
+  for (i = 1; i <= N; i++) {
+    for (j = 0; j < stages.length; j++) {
+      if (stages[j] == i) {
+        arr[i - 1]++;
+      }
+    }
+  }
+  console.log(arr);
+  let deno = stages.length;
+  const per = arr.map((x, i) => {
+    let devide = deno;
+    deno -= x;
+    return x / devide;
+  });
+  console.log(per);
+  const per2 = [...per];
+  let answer = [];
+  per2.sort((a, b) => {
+    return b - a;
+  });
+  console.log(per2);
+  console.log(per2.indexOf(per[0]));
+  for (i = 0; i < per.length; i++) {
+    answer.push(per2.indexOf(per[i]) + 1);
+  }
+  console.log(answer);
 }
--- algorithm/for RunJS.js
+++ algorithm/for RunJS.js
@@ -1,7 +1,20 @@
+function topsyTurvy(str, arr) {
 
-
-function isUnique(str) {
-console.log(str.split(""))
+ let answer =""
+ for(i=0; i<arr.length;i++){
+   let idx= arr.indexOf(i)
+   console.log(idx)
+   answer +=str[idx]
+ }
+ return answer
+ 
 }
+  
 
-isUnique('abcd')
+
+
+
+
+
+topsyTurvy('campcode', [4, 5, 6, 7, 0, 1, 2, 3]) //'codecamp'
+topsyTurvy('bsktcaurs', [4, 8, 7, 1, 6, 2, 5, 3, 0]) // 'starbucks'
