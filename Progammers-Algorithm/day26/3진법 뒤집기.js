97e544b0a274793694b9a47e3df0f028d476be56
leokim1178 <leo3179@naver.com>
2022-04-13 10:28:36 +0900

    day23-algorithm-26-1

--- Progammers-Algorithm/day26/3진법 뒤집기.js
+++ Progammers-Algorithm/day26/3진법 뒤집기.js
@@ -1,4 +1,14 @@
-function solution(nums) {
-  var answer = 0;
-  return answer;
+function solution(n) {
+  var answer = "";
+  for (i = 0; n / 3 ** i > 0; i++) {
+    answer += n % 3;
+    n = Math.floor(n / 3);
+  }
+  const result = answer
+    .split("")
+    .reverse()
+    .reduce((acc, cur, i) => {
+      return acc + cur * 3 ** i;
+    }, 0);
+  return result;
 }
