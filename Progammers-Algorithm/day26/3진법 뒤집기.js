700f89c4b55819708849da2de0263c7afb09d6af
leokim1178 <leo3179@naver.com>
2022-04-18 12:51:47 +0900

    algorithm-26

--- Progammers-Algorithm/day26/3진법 뒤집기.js
+++ Progammers-Algorithm/day26/3진법 뒤집기.js
@@ -12,3 +12,39 @@ function solution(n) {
     }, 0);
   return result;
 }
+
+a = 45; //45
+
+String(a);
+
+a.toString(3); //10진법을 N진수의 결과를 받아온다
+// '1200'
+
+b = "1200";
+
+parseInt(b, 3); // 45 // 3진법으로 변환
+
+function solution(n) {
+  console.log("변환 전: " + n);
+  n = n.toString(3);
+  console.log("변환 후: " + n);
+
+  let answer = "";
+  for (let i = n.length - 1; i >= 0; i--) {
+    answer += n[i];
+  }
+  console.log(answer);
+  //포문을 반대로....반전
+  return parseInt(answer, 3); // answer를 3진법으로 변환하겠다
+}
+
+function solution(n) {
+  n = n
+    .toString(3) //n을 3진법으로 변환
+    .split("")
+    .reverse()
+    .join("");
+
+  console.log(n);
+  return parseInt(n, 3);
+}
--- Progammers-Algorithm/day26/이진 변환 반복하기.js
+++ Progammers-Algorithm/day26/이진 변환 반복하기.js
@@ -21,3 +21,64 @@ function solution(s) {
 
   return [result, zero];
 }
+
+function solution(s) {
+  let count = 0; //총 시도한 횟수
+  let remove = 0; // 0이 제거된 총 횟수
+
+  //s가 "1"이 되지 않을 때까지, ("1"이 되면 중단)
+  while (s !== "1") {
+    count++;
+
+    //"1"만 담는 변수
+    let temp = "";
+    //"0"을 제거하는 반복문
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "0") {
+        //0을 발견했다면 temp 변수에 "0"을 넣지 않는다.
+        remove++;
+        continue;
+      }
+      //temp 에는 "1"만 담는다.
+      temp += s[i];
+    }
+    s = temp.length;
+    //s를 2진법으로 변환한 데이터의 결과가 "1"이라면 반복문이 종료
+    s = s.toString(2);
+    console.log(s, count, remove, temp);
+  }
+  return [count, remove];
+}
+
+let count = 0;
+function recursion() {
+  if (count >= 5) {
+    return;
+  }
+
+  count++;
+  console.log(count);
+  return recursion();
+}
+
+recursion(); //5
+//끊어주는 시점만 정해주면 while문같이 사용가능
+
+//재귀함수로 풀기
+function solution(s) {
+  let [count, remove] = [0, 0];
+
+  function recursion() {
+    if (s === "1") {
+      return [count, remove];
+    }
+    //0을 제거
+    remove += s.split("").filter((el) => el === "0").length;
+    //0이 제거된 1만 남은 문자열의 길이값을 2진법으로 변환
+    s = s.split("").filter((el) => el === "1").length;
+    s = s.toString(2);
+    count++;
+    return recursion();
+  }
+  return recursion();
+}
