c511bbfde6e2aadb16d9f9e9745a7844ae3a1139
leokim1178 <leo3179@naver.com>
2022-04-18 21:12:40 +0900

    main-project-transaction-isolation

--- homework/main-project/backend/src/apis/payment/payment.service.ts
+++ homework/main-project/backend/src/apis/payment/payment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+
+import { Connection, Repository } from 'typeorm';
 import { User } from '../user/entities/user.entity';
 import { Payment, PAYMENT_STATUS_ENUM } from './entities/payment.entity';
 
@@ -11,32 +12,58 @@ export class PaymentService {
         private readonly paymentRepository: Repository<Payment>,
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
+        private readonly connection: Connection,
     ) {}
 
     //결제정보 생성 및 저장
     async create({ impUid, amount, currentUser, merchantUid }) {
-        //1. Payment ㅌㅔ이블에 거래기록 1줄 생성
+        const queryRunner = await this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction('SERIALIZABLE');
+        //transaction 시작!
+        try {
+            //1. Payment 테이블에 거래기록 1줄 생성
+            const payment = this.paymentRepository.create({
+                impUid: impUid,
+                amount: amount,
+                user: currentUser,
+                merchantUid: merchantUid,
+                status: PAYMENT_STATUS_ENUM.PAYMENT,
+            });
 
-        const payment = this.paymentRepository.create({
-            impUid: impUid,
-            amount: amount,
-            user: currentUser,
-            merchantUid: merchantUid,
-            status: PAYMENT_STATUS_ENUM.PAYMENT,
-        });
-        await this.paymentRepository.save(payment);
-        //2. 유저 돈 찾아오기
-        const user = await this.userRepository.findOne({
-            where: { id: currentUser.id },
-        });
-        //3. 유저의 돈 업데이트
-        await this.userRepository.update(
-            { id: user.id },
-            { point: user.point + amount },
-        );
-        //4.최종결과를 프론트에 돌려주기
-        console.log('저장완료');
-        return payment;
+            await queryRunner.manager.save(payment);
+            // await this.paymentRepository.save(payment);
+
+            //2. 유저 돈 찾아오기
+            const user = await queryRunner.manager.findOne(
+                User, //
+                { id: currentUser.id },
+                { lock: { mode: 'pessimistic_write' } },
+            );
+            // const user = await this.userRepository.findOne({
+            //     where: { id: currentUser.id },
+            // });
+            //3. 유저의 돈 업데이트
+            // await this.userRepository.update(
+            //     { id: user.id },
+            //     { point: user.point + amount },
+            // );
+            const updatedUser = this.userRepository.create({
+                ...user,
+                point: user.point + amount,
+            });
+            await queryRunner.manager.save(updatedUser);
+
+            await queryRunner.commitTransaction();
+
+            //4.최종결과를 프론트에 돌려주기
+            console.log('저장완료');
+            return payment;
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 
     //DB의 결제정보 조회
@@ -54,29 +81,53 @@ export class PaymentService {
         cancelReason,
         impUid,
     }) {
-        //1. Payment 테이블에 취소기록 1줄 생성
+        const queryRunner = await this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction('SERIALIZABLE');
+        //transaction 시작!!
+        try {
+            //1. Payment 테이블에 취소기록 1줄 생성
+            const payment = this.paymentRepository.create({
+                impUid: impUid,
+                amount: -amount,
+                user: currentUser,
+                merchantUid: merchantUid,
+                cancelReason: cancelReason,
+                status: PAYMENT_STATUS_ENUM.CANCEL,
+            });
 
-        const result = this.paymentRepository.create({
-            impUid: impUid,
-            amount: -amount,
-            user: currentUser,
-            merchantUid: merchantUid,
-            cancelReason: cancelReason,
-            status: PAYMENT_STATUS_ENUM.CANCEL,
-        });
-        await this.paymentRepository.save(result);
+            const result = await queryRunner.manager.save(payment);
+            // await this.paymentRepository.save(result);
 
-        //2. 유저 돈 찾아오기
-        const user = await this.userRepository.findOne({
-            where: { id: currentUser.id },
-        });
+            //2. 유저 돈 찾아오기
+            const user = await queryRunner.manager.findOne(
+                User,
+                { id: currentUser.id },
+                { lock: { mode: 'pessimistic_write' } },
+            );
+            // const user = await this.userRepository.findOne({
+            //     where: { id: currentUser.id },
+            // });
 
-        //3. 유저의 돈 업데이트
-        await this.userRepository.update(
-            { id: user.id },
-            { point: user.point - amount },
-        );
-        //4. 저장 결과 돌려주기
-        return result;
+            //3. 유저의 돈 업데이트
+            const updatedUser = this.userRepository.create({
+                ...user,
+                point: user.point - amount,
+            });
+
+            await queryRunner.manager.save(updatedUser);
+
+            await queryRunner.commitTransaction();
+            // await this.userRepository.update(
+            //     { id: user.id },
+            //     { point: user.point - amount },
+            // );
+            //4. 저장 결과 돌려주기
+            return result;
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 }
