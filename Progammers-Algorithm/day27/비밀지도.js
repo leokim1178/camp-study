2e7f4f1d372e349ec60880d3b0e9b4e03e82c414
leokim1178 <leo3179@naver.com>
2022-04-19 15:11:02 +0900

    algorithm-27

--- Progammers-Algorithm/day27/비밀지도.js
+++ Progammers-Algorithm/day27/비밀지도.js
@@ -35,3 +35,25 @@ function solution(n, arr1, arr2) {
 }
 
 //vertical bar 공부
+
+function solution(n, arr1, arr2) {
+  const answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    answer[i] = "";
+
+    //지도1을 2진법으로 변환한 데이터를 저장
+    const map1 = arr1[i].toString(2).padStart(n, "0");
+    //지도2를 2진법으로 변환한 데이터를 저장
+    const map2 = arr2[i].toString(2).padStart(n, "0");
+
+    for (let j = 0; j < map1.length; j++) {
+      //둘 중 하나라도 벽이기 때문에, 전체 지도에서도 벽이다.
+      if (map1[j] === "1" || map2[j] === "1") {
+        answer[i] += "#";
+      } else if (map1[j] === "0" && map2[j] === "0") {
+        answer[i] += " ";
+      }
+    }
+  }
+  return answer;
+}
