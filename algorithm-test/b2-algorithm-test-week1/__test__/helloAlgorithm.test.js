0c21e2e06167f4ec4b39cfaf9c3d1adb7da57e6c
leokim1178 <leo3179@naver.com>
2022-04-23 00:52:47 +0900

    arrange-folders

--- algorithm-test/b2-algorithm-test-week1/__test__/helloAlgorithm.test.js
+++
@@ -1,7 +0,0 @@
-const helloalte = require("../src/01_helloAlgorithm");
-
-describe("알고리즘 테스트 익히기", () => {
-  test("함수는 항상 true를 리턴해야 합니다", () => {
-    expect(helloalte()).toEqual(true);
-  });
-});
