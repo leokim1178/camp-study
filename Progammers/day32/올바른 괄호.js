8b14e8bcf27f861da2d00d8b08b20bb068d8e16d
leokim1178 <leo3179@naver.com>
2022-04-26 12:59:17 +0900

    algorithm-day32-final

--- Progammers/day32/올바른 괄호.js
+++ Progammers/day32/올바른 괄호.js
@@ -5,5 +5,46 @@ function solution(s) {
 
   return s.length === 0;
 }
+//효율성 테스트 실패(시간초과)
 
-solution("(())()");
+function solution(s) {
+  const limit = s.length / 2;
+  for (let i = 0; i < limit; i++) {
+    s = s.replace("()", "");
+  }
+  return s === "";
+}
+//효율성 테스트 실패(시간초과)
+
+function solution(s) {
+  if (s[0] === ")" || s[s.length - 1] === "(") return false;
+  let depth = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      //열려있다면 +1
+      depth++;
+    } else if (s[i] === ")") {
+      //닫혀있다면 -1
+      depth--;
+      //닫혀있는게 더 많다면 먼저 false를 리턴한다
+      if (depth < 0) {
+        return false;
+      }
+    }
+  }
+
+  return depth == 0;
+}
+
+function solution(s) {
+  if (s[0] === ")" || s[s.length - 1] === "(") return false;
+
+  let fail = false;
+  const answer = s.split("").reduce((acc, cur) => {
+    if (acc < 0) {
+      fail = true;
+    }
+    return acc + (cur === "(" ? 1 : -1);
+  }, 0);
+  return answer === 0 && !fail;
+}
--- Progammers/day32/최댓값과 최솟값.js
+++ Progammers/day32/최댓값과 최솟값.js
@@ -7,5 +7,12 @@ function solution(s) {
   return answer;
 }
 
+function solution(s) {
+  let string = s.split(" ");
+
+  const answer = [Math.min(...string), Math.max(...string)].join(" ");
+  return answer;
+}
+///Number로 바꿔줄 필요 없음
 solution("1 2 3 4");
 solution("-1 -2 -3 -4");
